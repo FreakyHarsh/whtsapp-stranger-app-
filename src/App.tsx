8d798728c1326660d4cd484c3938a99208ed0c68
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-01-05 23:10:45 +0530

    feat: global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
@@ -23,15 +20,6 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => <Home />;
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,92 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonItem,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import { Button, TextField } from '@material-ui/core';
+import React, { useState } from 'react';
 import './Home.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { useStore } from '../store/store';
+import { State } from '../types/state';
+import { Actions } from '../types/actions';
 
 const Home: React.FC = () => {
+  const [number, setNumber] = useState<string | null>(null);
+  const [msg, setMsg] = useState<string | null>(null);
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#2DD36F',
+      },
+    },
+  });
+  const {
+    state: { phoneNumber, message },
+    dispatch,
+  } = useStore();
+  const sendMsg = () => {
+    window.open(`https://api.whatsapp.com/send?phone=91${number}&text=${msg}`);
+  };
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar color='success'>
+          <IonTitle>WhatsApp stranger</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent style={{ background: 'red' }}>
+        <ThemeProvider theme={theme}>
+          <IonGrid>
+            <IonRow>
+              <IonCol sizeMd='6' offsetMd='3'>
+                <IonCard>
+                  <IonCardContent className='ion-padding'>
+                    <div className='ion-margin-top'></div>
+                    <TextField
+                      type='number'
+                      value={phoneNumber}
+                      variant='outlined'
+                      placeholder='Enter Number'
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        number!.length <= 10 &&
+                          dispatch({ type: Actions.setPhoneNumber, payload: value });
+                      }}
+                      fullWidth
+                    />
+                    <div className='ion-margin-top'></div>
+                    <TextField
+                      variant='outlined'
+                      placeholder='Message...'
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        dispatch({ type: Actions.setMessage, payload: value });
+                      }}
+                      fullWidth
+                    />
+                    <div className='ion-text-end ion-padding-top'>
+                      <Button variant='outlined' color='primary' size='large' onClick={sendMsg}>
+                        Send
+                      </Button>
+                    </div>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </ThemeProvider>
       </IonContent>
     </IonPage>
   );
